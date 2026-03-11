fed58359be91fb9378162754c4b6325bdd4e8f74
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-03-11 14:25:12 +0900

    feat: 설명을 입력 받는 기능 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/taskcard/CreateTaskCardModal.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/taskcard/CreateTaskCardModal.kt
@@ -1,7 +1,10 @@
 package woowacourse.kanban.board.ui.taskcard
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -12,21 +15,36 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import woowacourse.kanban.board.domain.Task
 
 @Composable
-fun CreateTaskCardModal() {
+fun CreateTaskCardModal(modifier: Modifier = Modifier) {
     var title by remember { mutableStateOf("") }
     var isTitleError by remember { mutableStateOf(false) }
-    TitleField(
-        title = title,
-        onValueChange = {
-            title = it
-            isTitleError = !Task.isValidTitle(title)
-        },
-        isError = isTitleError,
-        modifier = Modifier.background(Color.White),
-    )
+    var content by remember { mutableStateOf("") }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        TitleField(
+            title = title,
+            onValueChange = {
+                title = it
+                isTitleError = !Task.isValidTitle(title)
+            },
+            isError = isTitleError,
+            modifier = Modifier.background(Color.White),
+        )
+        ContentField(
+            content = content,
+            onValueChange = { content = it },
+            modifier = Modifier
+                .background(Color.White)
+                .heightIn(min = 144.dp),
+        )
+    }
+
 }
 
 @Composable
@@ -48,8 +66,25 @@ private fun TitleField(
     }
 }
 
+@Composable
+private fun ContentField(
+    content: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier) {
+        Text("설명")
+        TextField(
+            modifier = modifier,
+            value = content,
+            onValueChange = onValueChange,
+            placeholder = { Text("태스크에 대한 자세한 설명을 입력하세요") }
+        )
+    }
+}
+
 @Preview
 @Composable
 private fun PreviewCreateTaskCardModal() {
-    CreateTaskCardModal()
+    CreateTaskCardModal(modifier = Modifier.background(Color(0xFFE5E7EB)))
 }
