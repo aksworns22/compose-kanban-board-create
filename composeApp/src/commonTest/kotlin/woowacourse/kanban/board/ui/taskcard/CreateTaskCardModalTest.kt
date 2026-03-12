567384ebaa88ceaf06dd893610f19a79e676662e
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-03-12 15:39:13 +0900

    test: 제목을 입력하지 않으면 생성 버튼이 활성화되지 않는지 검증

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/taskcard/CreateTaskCardModalTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/taskcard/CreateTaskCardModalTest.kt
@@ -1,9 +1,11 @@
 package woowacourse.kanban.board.ui.taskcard
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.printToLog
 import androidx.compose.ui.test.runComposeUiTest
 import kotlin.test.Test
 
@@ -21,5 +23,18 @@ class CreateTaskCardModalTest {
         onNodeWithText("태스크 제목을 입력하세요").performTextInput(typedTitle)
         onNodeWithText(typedTitle).performTextClearance()
         onNodeWithText("제목을 입력해주세요.").assertExists()
+    }
+
+    @Test
+    fun `제목을 입력하지 않으면 생성 버튼이 활성화되지 않는다`() = runComposeUiTest {
+        setContent {
+            CreateTaskCardModal(authors = listOf("다이노", "페임스"))
+        }
+
+        val typedTitle = "지워질 제목입니다"
+
+        onNodeWithText("태스크 제목을 입력하세요").performTextInput(typedTitle)
+        onNodeWithText(typedTitle).performTextClearance()
+        onNodeWithText("생성").assertIsNotEnabled()
     }
 }
