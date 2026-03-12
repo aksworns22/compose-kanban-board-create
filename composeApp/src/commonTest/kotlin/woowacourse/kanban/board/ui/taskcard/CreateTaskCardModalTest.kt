15cfd33c8da543052b1ad0c56f6813cc34f0c002
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-03-12 15:36:59 +0900

    test: 제목을 입력하지 않으면 에러 문구가 노출되는지 검증

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/taskcard/CreateTaskCardModalTest.kt
@@ -0,0 +1,25 @@
+package woowacourse.kanban.board.ui.taskcard
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextClearance
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class CreateTaskCardModalTest {
+
+    @Test
+    fun `제목을 입력하지 않으면 에러 문구가 노출된다`() = runComposeUiTest {
+        setContent {
+            CreateTaskCardModal(authors = listOf("다이노", "페임스"))
+        }
+
+        val typedTitle = "지워질 제목입니다"
+
+        onNodeWithText("태스크 제목을 입력하세요").performTextInput(typedTitle)
+        onNodeWithText(typedTitle).performTextClearance()
+        onNodeWithText("제목을 입력해주세요.").assertExists()
+    }
+}
