5a687ea764b15ea1094eb9e12b73f0b4eb7d7b19
aksworns22 <wkdeogks17@gmail.com>
2026-03-19 16:54:46 +0900

    test: x 버튼을 클릭하면 다이어로그가 사라지는 지 검증

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/KanbanBoardTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/KanbanBoardTest.kt
@@ -1,8 +1,11 @@
 package woowacourse.kanban.board.ui
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import androidx.compose.ui.test.runComposeUiTest
 import kotlin.test.Test
@@ -11,10 +14,20 @@ import kotlin.test.Test
 class KanbanBoardTest {
     @Test
     fun `새 태스크 생성 버튼을 누르면 생성 모달(다이어로그)를 표시한다`() = runComposeUiTest {
-        val kanbanBoardState = KanbanBoardState(isNewTaskButtonClicked = true)
+        val kanbanBoardState = KanbanBoardState(isCreateTaskDialogDisplayed = true)
         setContent {
             KanbanBoardContent(kanbanBoardState = kanbanBoardState)
         }
         onNodeWithContentDescription("새 태스크 생성").isDisplayed()
+    }
+
+    @Test
+    fun `x 버튼을 선택하면 태스크 생성 모달(다이어로그)가 사라진다`() = runComposeUiTest {
+        setContent {
+            val kanbanBoardState = remember { KanbanBoardState(isCreateTaskDialogDisplayed = true) }
+            KanbanBoardContent(kanbanBoardState = kanbanBoardState)
+        }
+        onNodeWithContentDescription("x 버튼").performClick()
+        onNodeWithContentDescription("새 태스크 생성").assertDoesNotExist()
     }
 }
